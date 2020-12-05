469e57b84b095288623c76a4f2bf8ae2ee9f54f6
CedarBlocks <countrycedarworks@gmail.com>
2020-12-04 18:24:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const RPC = require('discord-rpc');
+const rpc = new RPC.Client({
+    transport: 'ipc'
+});
+let song = require('./np.json');
+
+function refresh() {
+    delete require.cache[require.resolve('./np.json')]
+    return song = require('./np.json');
+}
+
+rpc.on('ready', () => {
+    try {
+        setActivity(song)
+    } catch (error) {
+        console.error(error);
+    };
+
+    setInterval(() => {
+        try {
+            fs.readFile('./np.json', 'utf8', (err, jsonString) => {
+                if (err) {
+                    console.log("File read failed:", err)
+                    return
+                }
+                if (JSON.stringify(song) !== jsonString) {
+                    refresh()
+                    setActivity(song)
+                };
+            });
+        } catch (error) {
+            console.error(error);
+        };
+    }, 2e3);
+
+    console.log('Connected!');
+});
+
+function setActivity(song) {
+    rpc.setActivity({
+        details: `🎶 ${song.track.title}`,
+        state: `👤 ${song.track.artistsString}`,
+        startTimestamp:  Date.now(),
+        largeImageKey: 'pretzel',
+        largeImageText: 'Pretzel Rocks',
+        smallImageKey: smallKey(song),
+        smallImageText: smallText(song)
+    });
+};
+
+function smallKey(song) {
+    if (song.player.playing === true) {
+        return 'play'
+    } else {
+        return 'pause'
+    }
+};
+
+function smallText(song) {
+    if (song.player.playing === true) {
+        return 'Playing'
+    } else {
+        return 'Paused'
+    }
+};
+
+rpc.login({
+    clientId: '784497631186649108'
+});
